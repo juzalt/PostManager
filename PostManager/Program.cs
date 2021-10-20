dcca3d49ea0b1ccaea3303975e6e46b24d29c806
Julian Zaltron <jzaltron@k12.com>
2021-10-20 00:54:24 -0300

    First Commit

---
+++ PostManager/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PostManager
+{
+    class Program
+    {
+        private Program postManager;
+        private object[] listOfPosts;
+
+        public void ViewPosts()
+        {
+            Console.WriteLine("Viewing Posts");
+            postManager.AwaitForACommand();
+        }
+
+        public void SelectPost()
+        {
+            Console.WriteLine("Viewing an individual post");
+            postManager.AwaitForACommand();
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        private void GiveIntroduction()
+        {
+            Console.WriteLine("Welcome to the post manager! :)");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            postManager.AwaitForACommand();
+        }
+
+        private void AwaitForACommand()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Would you like to [list] the posts, [select] a post, or [exit]?");
+            string command = Console.ReadLine();
+            postManager.ActUponACommand(command);
+        }
+
+        private void ActUponACommand(string command)
+        {
+            switch (command)
+            {
+                case "list":
+                    postManager.ViewPosts();
+                    break;
+
+                case "select":
+                    postManager.SelectPost();
+                    break;
+
+                case "exit":
+                    postManager.Exit();
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program instanceOfProgram = new Program();
+            instanceOfProgram.postManager = instanceOfProgram;
+
+            instanceOfProgram.postManager.GiveIntroduction();
+        }
+    }
+}
